79dec0071d2710ba40d34d071684cd2264b6099c
Kevinardi17.com <Kevinardi17.com>
2024-10-09 01:06:03 +0700

    .

--- Modul 5/Modul 5 pertanyaan 2/script.js
+++ Modul 5/Modul 5 pertanyaan 2/script.js
@@ -2,16 +2,16 @@ function submitOrder() {
     const checkboxes = document.querySelectorAll('input[name="menu"]:checked');
     let selectedItems = [];
 
-    checkboxes.forEach((checkbox) => {
+    Array.from(checkboxes).forEach((checkbox) => {
         selectedItems.push(checkbox.value);
     });
 
     if (selectedItems.length > 0) {
-        console.log("Pesanan Anda:");
+        console.log("Pesanan Anda: ");
         selectedItems.forEach((item) => {
             console.log(item);
         });
     } else {
-        console.log("Tidak ada pesanan yang dipilih.");
+        console.log("Anda belum memesan makanan. Silahkan pesan terlebih dahulu.");
     }
 }
